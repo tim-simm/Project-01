5676892a7783613ab04550d5bf79b5682d558958
tim-simm <tim.lamich@gmx.de>
2024-05-06 18:39:37 +0200

    Navbar

---
+++ project01/src/components/navbar.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'; //imcr
+
+class Navbar extends Component { //cc
+    state = {  } 
+    render() { 
+        return <nav>Navigator</nav>;
+    }
+}
+ 
+export default Navbar;
