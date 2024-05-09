be503803219ae56b392a3bb14ca4a1431a78b57a
lamit03 <lamit03@zedat.fu-berlin.de>
2024-05-09 18:50:17 +0200

    Homepage V1.0

--- project01/src/App.jsx
+++ project01/src/App.jsx
@@ -8,9 +8,9 @@ class App extends Component {
         return <React.Fragment>
                     <Navbar/>
                     <div className="product-container">
-                        <Product/>
-                        <Product/>
-                        <Product/>
+                        <Product title="Studium" image="assets/img/studium.jpg" describtion="Informatik B. Sc. an der Freien Universität Berlin"/>
+                        <Product title="Letzte Projekte" image="assets/img/projekte.jpg" describtion="Softwareprojekt Coding IxD"/>
+                        <Product title="Beruf" image="assets/img/beruf.jpg" describtion="Werkstudent in der Technischen Kundenbetreuung bei Projektron GmbH"/>
                     </div>
                 </React.Fragment>;
     }
--- project01/src/components/products.jsx
+++ project01/src/components/products.jsx
@@ -5,10 +5,10 @@ class Product extends Component {
     state = {  } 
     render() { 
         return <div className="card" styles="width: 18rem;">
-        <img src="assets/img/test1.jpg" className="card-img-top" alt="..."/>
+        <img src={this.props.image} className="card-img-top" alt="..."/>
         <div className="card-body">
-          <h5 className="card-title">Card title</h5>
-          <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <h5 className="card-title">{this.props.title}</h5>
+          <p className="card-text">{this.props.describtion}</p>
           <a href="/" className="btn btn-primary">Go somewhere</a>
         </div>
       </div>;
